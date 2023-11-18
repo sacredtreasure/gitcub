bf78da29f2d7ccc3f77eb897b12cd2f084336b65
sacredtreasure <selfharm777@protonmail.com>
2023-11-18 02:13:09 +0100

    init of pull request prompt

--- src/pr.rs
+++ src/pr.rs
@@ -1,4 +1,23 @@
+use clap::Subcommand;
+
+#[derive(Subcommand)]
+
 pub enum Pr {
+    /// spawn a pull
     Create,
+    /// list pulls
     List,
+}
+
+impl Pr {
+    pub fn exec(&self) {
+        match self {
+            Pr::Create => {
+                println!("Pull Request Created!")
+            }
+            Pr::List => {
+                println!("List of Pull Requests:")
+            }
+        }
+    }
 }
